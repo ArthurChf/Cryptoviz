516a3bd0b9dd13419930495b7f3ec088fc927ac4
Antoine Romand <antoine.romand@epitech.eu>
2023-08-29 22:38:10 +0200

    Create modules for online scrapper and rss feed

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,12 @@
+import { RssFeedModule } from './online-scrapper/rss-feed/rss-feed.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from '@app/database';
+import { OnlineScrapperModule } from './online-scrapper/online-scrapper.module';
 
 @Module({
-    imports: [DatabaseModule],
+    imports: [DatabaseModule, RssFeedModule, OnlineScrapperModule],
     controllers: [AppController],
     providers: [AppService],
 })
