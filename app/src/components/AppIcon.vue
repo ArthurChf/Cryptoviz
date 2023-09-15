4395662f2695233a33bc7be60fb1b2439e0f4eee
Arthur <chaff.arthur@gmail.com>
2023-09-15 10:36:35 +0200

    Icon component

---
+++ app/src/components/AppIcon.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import BaseIcons from '@assets/icons/base.svg';
+
+defineProps<{
+    name: string;
+}>();
+</script>
+
+<template>
+    <svg class="inline-flex">
+        <use :href="`${BaseIcons}#${name}`" />
+    </svg>
+</template>
--- app/src/views/Dashboard.vue
+++ app/src/views/Dashboard.vue
@@ -1,9 +1,17 @@
 <template>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    <div>
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+        <AppIcon :name="IconEnum.STATUS_SUCCESS" class="h-7 w-7 fill-teal-500" />
+        <AppIcon :name="IconEnum.STATUS_ERROR" class="h-7 w-7 fill-red-500" />
+        <AppIcon :name="IconEnum.STATUS_NEUTRAL" class="h-7 w-7 fill-slate-500" />
+        <AppIcon :name="IconEnum.ADD" class="h-7 w-7 fill-neutral-500" />
+        <AppIcon :name="IconEnum.MORE" class="h-7 w-7 fill-neutral-500" />
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<script setup lang="ts">
+import AppIcon from '@components/AppIcon.vue';
+import { IconEnum } from '@enums/IconEnum';
+</script>
