dcfe35e6e9e0977255ac86aba365f3ebf9ed1c06
Arthur <chaff.arthur@gmail.com>
2023-09-15 09:49:38 +0200

    change architecture

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -10,11 +10,14 @@ export default defineConfig({
     resolve: {
         alias: [
             { find: '@assets', replacement: fileURLToPath(new URL('./src/assets', import.meta.url)) },
+            { find: '@classes', replacement: fileURLToPath(new URL('./src/classes', import.meta.url)) },
             { find: '@components', replacement: fileURLToPath(new URL('./src/components', import.meta.url)) },
             { find: '@composables', replacement: fileURLToPath(new URL('./src/composables', import.meta.url)) },
             { find: '@enums', replacement: fileURLToPath(new URL('./src/enums', import.meta.url)) },
             { find: '@layouts', replacement: fileURLToPath(new URL('./src/layouts', import.meta.url)) },
+            { find: '@repositories', replacement: fileURLToPath(new URL('./src/repositories', import.meta.url)) },
             { find: '@router', replacement: fileURLToPath(new URL('./src/router', import.meta.url)) },
+            { find: '@stores', replacement: fileURLToPath(new URL('./src/stores', import.meta.url)) },
             { find: '@types', replacement: fileURLToPath(new URL('./src/types', import.meta.url)) },
             { find: '@views', replacement: fileURLToPath(new URL('./src/views', import.meta.url)) }
         ]
