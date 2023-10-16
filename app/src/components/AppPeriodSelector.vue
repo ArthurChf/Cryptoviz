d8b1da787c55005881407d7ac603b2fb6660a437
Arthur <chaff.arthur@gmail.com>
2023-10-16 22:24:55 +0200

    store selected period

--- app/src/components/AppPeriodSelector.vue
+++ app/src/components/AppPeriodSelector.vue
@@ -6,17 +6,19 @@
 
 <script setup lang="ts">
 import { PeriodSelectorEnum } from '@enums/period-selector/PeriodSelectorEnum';
-import dayjs from 'dayjs';
 import { ref } from 'vue';
+import { useAppStore } from '@stores/appStore';
+// import dayjs from 'dayjs';
 
+const appStore = useAppStore();
 const selectedPeriod = ref<PeriodSelectorEnum>(PeriodSelectorEnum.ONE_DAY);
 
 function selectPeriod(period: PeriodSelectorEnum): void {
-    if(period === PeriodSelectorEnum.ONE_DAY) console.log(dayjs().subtract(1, 'day').startOf('day').format('DD/MM/YYYY HH:mm:ss'));
+    /* if(period === PeriodSelectorEnum.ONE_DAY) console.log(dayjs().subtract(1, 'day').startOf('day').format('DD/MM/YYYY HH:mm:ss'));
     else if(period === PeriodSelectorEnum.SEVEN_DAYS) console.log(dayjs().subtract(7, 'days').startOf('day').format('DD/MM/YYYY HH:mm:ss'));
     else if(period === PeriodSelectorEnum.ONE_MONTH) console.log(dayjs().subtract(1, 'month').startOf('day').format('DD/MM/YYYY HH:mm:ss'));
-    else console.log(dayjs().subtract(1, 'year').startOf('day').format('DD/MM/YYYY HH:mm:ss'));
-
+    else console.log(dayjs().subtract(1, 'year').startOf('day').format('DD/MM/YYYY HH:mm:ss')); */
+    appStore.selectPeriod(period);
     selectedPeriod.value = period;
 }
 </script>
