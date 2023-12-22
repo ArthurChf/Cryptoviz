9f7b3db82ba03889824186b5e5486e0b9c904e1f
Arthur <chaff.arthur@gmail.com>
2023-12-22 12:36:36 +0100

    send binance message to kafka

--- api/apps/scrapper/src/binance-api/binance-api.module.ts
+++ api/apps/scrapper/src/binance-api/binance-api.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { BinanceApiService } from './binance-api.service';
+import { KafkaService } from '@/libs/kafka/src/kafka.service';
 
 @Module({
-    providers: [BinanceApiService]
+    providers: [BinanceApiService, KafkaService]
 })
 export class BinanceApiModule {}
