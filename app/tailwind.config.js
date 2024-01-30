e633133f08a5398178def51e279636caa0c6f681
Arthur <chaff.arthur@gmail.com>
2024-01-30 22:44:46 +0100

    set custom colors tailwind

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -5,6 +5,15 @@ export default {
     ],
     plugins: [],
     theme: {
-        extend: {}
-    }
+        colors: {
+            'title': '#fdfeff',
+            'subtitle': '#48494e',
+            'background': '#131720',
+            'container': '#161d27',
+            'active': '#10b569'
+        },
+        transitionDuration: {
+            '200': '200ms'
+        }
+    },
 };
