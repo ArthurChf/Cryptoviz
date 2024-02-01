df0d8770a386467746379be25e65f419086fc9fa
Arthur <chaff.arthur@gmail.com>
2024-02-01 22:02:43 +0100

    Display trade currencies images

--- app/src/components/dashboard/RecentActivitiesCard.vue
+++ app/src/components/dashboard/RecentActivitiesCard.vue
@@ -4,15 +4,16 @@
         <div class="overflow-y-auto overflow-x-hidden h-[295px] scrollbar">
             <table ref="recentActivitiesTable" class="table-fixed w-full select-none">
                 <tr class="border-b-2 border-subtitle/5 text-subtitle text-md">
-                    <th class="text-left pl-3 pb-3 font-semibold w-1/12">Symbol</th>
-                    <th class="text-left pb-3 font-semibold w-3/12">Currency</th>
+                    <th class="text-left pl-3 pb-3 font-semibold w-3/12">Currency</th>
                     <th class="text-left pb-3 font-semibold w-3/12">Transaction</th>
                     <th class="text-left pb-3 font-semibold w-3/12">ID</th>
                     <th class="text-right pr-3 pb-3 font-semibold w-2/12">Date</th>
                 </tr>
                 <tr v-for="transaction in lastTransactions" :key="transaction.id" class="transition duration-200 hover:bg-background">
-                    <td class="text-left pl-3 py-5 font-medium text-title">{{ transaction.currencySymbol }}</td>
-                    <td class="text-left py-5 font-medium text-subtitle">{{ transaction.currencyName }}</td>
+                    <td class="flex items-center gap-4 py-5 pl-3 font-medium text-subtitle">
+                        <AppImage :name="getCurrency(transaction.currencySymbol).image" size="35" />
+                        <span>{{ transaction.currencyName }}</span>
+                    </td>
                     <td class="text-left py-5 font-medium text-title">{{ transaction.amount }}</td>
                     <td class="text-left py-5 font-medium text-subtitle">#{{ transaction.id }}</td>
                     <td class="text-right pr-3 py-5 font-medium text-subtitle">{{ transaction.date }}</td>
@@ -27,6 +28,8 @@ import AppContainer from '@/components/AppContainer.vue';
 import { ref } from 'vue';
 import { useAutoAnimate } from '@formkit/auto-animate/vue';
 import type { Transaction } from '@/interfaces/Transaction';
+import { getCurrency } from '@/utils/getCurrency';
+import AppImage from '@/components/AppImage.vue';
 
 const [recentActivitiesTable] = useAutoAnimate();
 
