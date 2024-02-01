bf69b81774d385898f02cd5273a985337292b305
Arthur <chaff.arthur@gmail.com>
2024-02-01 20:29:29 +0100

    Add recent activities card

--- app/src/views/Dashboard.vue
+++ app/src/views/Dashboard.vue
@@ -3,7 +3,7 @@
         <div class="w-9/12 flex flex-col gap-6">
             <OverviewCard />
             <PriceTrendCard />
-            <AppContainer>3</AppContainer>
+            <RecentActivitiesCard />
         </div>
         <div class="flex flex-col gap-6 flex-1">
             <AppContainer>1</AppContainer>
@@ -16,4 +16,5 @@
 import AppContainer from '@/components/AppContainer.vue';
 import OverviewCard from '@/components/dashboard/OverviewCard.vue';
 import PriceTrendCard from '@/components/dashboard/PriceTrendCard.vue';
+import RecentActivitiesCard from '@/components/dashboard/RecentActivitiesCard.vue';
 </script>
