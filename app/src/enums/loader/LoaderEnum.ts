e76da8f2bd37cd8ec66773d58fec258bc53551e3
Arthur <chaff.arthur@gmail.com>
2024-02-02 20:16:59 +0100

    Update loader component and remove useless files

--- app/src/enums/loader/LoaderEnum.ts
+++
@@ -1,4 +0,0 @@
-export enum LoaderEnum {
-    CIRCULAR = 'CIRCULAR',
-    DOTS = 'DOTS'
-}
--- app/src/interfaces/loader/LoaderPropsInterface.ts
+++ app/src/interfaces/loader/LoaderPropsInterface.ts
@@ -1,8 +1,4 @@
-import type { LoaderEnum } from '@enums/loader/LoaderEnum';
-
 export interface LoaderPropsInterface {
-    color: string;
     size?: string;
     width?: string;
-    type: LoaderEnum;
 }
