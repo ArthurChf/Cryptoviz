67d1a300d7b1953ad3242946923d833e9cf7aff9
Arthur <chaff.arthur@gmail.com>
2024-02-03 16:32:07 +0100

    Add Tooltip component

---
+++ app/src/components/AppTooltip.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="group relative inline-block">
+        <slot />
+        <span class="animate-tooltip hidden absolute pointer-events-none text-title text-center py-2 px-3 rounded-md bg-background border border-2 border-subtitle/5 shadow-xl w-max group-hover:inline-flex" :class="`tooltip--${position}`">{{ text }}</span>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps<{
+    text: string;
+    position: 'top' | 'right' | 'bottom' | 'left';
+}>();
+
+const translateProperty = computed(() => {
+    if (props.position === 'top') return 'translate(-50%, -100%)';
+    else if (props.position === 'right') return 'translate(100%, -50%)';
+    else if (props.position === 'bottom') return 'translate(-50%, 100%)';
+    return 'translate(-100%, -50%)';
+});
+const transformOriginProperty = computed(() => {
+    if (props.position === 'top') return 'left';
+    else if (props.position === 'right') return 'right';
+    else if (props.position === 'bottom') return '0';
+    return 'left';
+});
+</script>
+
+<style scoped lang="css">
+.tooltip--top {
+    top: -10px;
+    left: 50%;
+}
+.tooltip--right {
+    right: -10px;
+    top: 50%;
+}
+.tooltip--bottom {
+    bottom: -10px;
+    left: 50%;
+}
+.tooltip--left {
+    left: -10px;
+    top: 50%;
+}
+
+.animate-tooltip {
+    transform-origin: v-bind(transformOriginProperty);
+    transform: scale(1) translate(-50%, 100%);
+    will-change: transform, opacity;
+    animation: animation-tooltip 0.3s forwards ease-in-out;
+}
+
+@keyframes animation-tooltip {
+    0% {
+        opacity: 0;
+        transform-origin: v-bind(transformOriginProperty);
+        transform: scale(0.95) v-bind(translateProperty);
+    }
+    40% {
+        opacity: 1;
+        transform-origin: v-bind(transformOriginProperty);
+        transform: scale(1.05) v-bind(translateProperty);
+    }
+    100% {
+        opacity: 1;
+        transform-origin: v-bind(transformOriginProperty);
+        transform: scale(1) v-bind(translateProperty);
+    }
+}
+</style>
