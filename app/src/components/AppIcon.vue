422e6fc05dc09e3e97398613a303945926e125f6
Arthur <chaff.arthur@gmail.com>
2024-02-04 13:31:18 +0100

    AppIcon fix

--- app/src/components/AppIcon.vue
+++ app/src/components/AppIcon.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="select-none inline-flex shrink-0">
-        <svg :style="`height: ${size}px; width: ${size}px; transform: scale(2.5);`" class="transition duration-200" :class="iconLoading ? 'opacity-0' : 'opacity-100'" @load="iconLoading = false">
+        <svg :style="`height: ${size}px; width: ${size}px; transform: scale(2.5);`" class="transition duration-200" :class="iconLoading ? 'opacity-0' : 'opacity-100'">
             <use :href="`${Icons}#${name}`" />
         </svg>
     </div>
@@ -9,11 +9,16 @@
 <script setup lang="ts">
 import Icons from '@/assets/icons.svg';
 import type { IconEnum } from '@/enums/IconEnum';
-import { ref } from 'vue';
+import { nextTick, onMounted, ref } from 'vue';
 
 defineProps<{
     size: string;
     name: IconEnum;
 }>();
 const iconLoading = ref(true);
+
+onMounted(async () => {
+    await nextTick();
+    iconLoading.value = false;
+});
 </script>
