88149adf2b332ad2995e609067c6facaa3c49ec2
Antoine Romand <antoine.romand@epitech.eu>
2024-02-07 22:58:43 +0100

    Add punkt (nlp) in docker, add extract crypto and sentiment (test)

--- python-scripts/src/process/news/etl_news_transform.py
+++ python-scripts/src/process/news/etl_news_transform.py
@@ -1,17 +1,35 @@
 import pandas as pda
+import textblob as blob
+import html 
+from utils.cryptodata import cryptocurrencies_keys
 
 def _analyze_content_sentiment(data):
-    pass
+    content = html.unescape(data)
+    blob_content = blob.TextBlob(content.lower())
+    if content is None or content == "":
+        return 0
+    sentiment_polarity = blob_content.sentiment.polarity
+    normalize_sentiment = (sentiment_polarity + 1) * 50
+    return normalize_sentiment
 
 def _extract_cryptocurrency_symbols(data):
-    pass
+    content = html.unescape(data)
+    if content is None:
+        return []
+    blob_content = blob.TextBlob(content.lower())
+    found_symbols = set()
+    for word in blob_content.words:
+        if word in cryptocurrencies_keys:
+            found_symbols.add(cryptocurrencies_keys[word])
+    return list(found_symbols)
 
 def generate_dataframe_from_news(data):
     return pda.DataFrame(data)
 
 def transform_news_data(data):
     news_dataframe = generate_dataframe_from_news(data)
-    news_dataframe['sentiment'] = _analyze_content_sentiment(news_dataframe['content'])
-    news_dataframe['cryptocurrency_symbols'] = _extract_cryptocurrency_symbols(news_dataframe['content'])
-    return news_dataframe.itertuples(index=False, name=None)
+    news_dataframe['sentiment'] = news_dataframe['content'].apply(_analyze_content_sentiment)
+    news_dataframe['cryptocurrencies'] = news_dataframe['content'].apply(_extract_cryptocurrency_symbols)
+    news_df_filtered = news_dataframe[news_dataframe['cryptocurrencies'].apply(lambda x: len(x) > 0)]
+    return news_df_filtered.itertuples(index=False, name=None)
 
--- python-scripts/utils/cryptodata.py
+++ python-scripts/utils/cryptodata.py
@@ -10,6 +10,27 @@ cryptocurrencies = [
     'auction'
 ]
 
+cryptocurrencies_keys = {
+    'bitcoin': 'btc',
+    'ethereum': 'eth',
+    'solana': 'sol',
+    'ripple': 'xrp',
+    'chainlink': 'link',
+    'pepe': 'pepe',
+    'tron': 'trx',
+    'floki': 'floki',
+    'auction': 'auction',
+    'btc': 'btc',
+    'eth': 'eth',
+    'sol': 'sol',
+    'xrp': 'xrp',
+    'link': 'link',
+    'pepe': 'pepe',
+    'trx': 'trx',
+    'floki': 'floki',
+    'auction': 'auction'
+}
+
 references = ['usdt']
 
 crypto_pairs = {
@@ -17,3 +38,4 @@ crypto_pairs = {
     for crypto in cryptocurrencies
     for ref in references
 }
+
