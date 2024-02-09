8e015fb80b5749a2a3b000b36c674b993fb66a53
Arthur <chaff.arthur@gmail.com>
2024-02-09 01:06:57 +0100

    Handle client subscriptions in memory

--- api/apps/data/src/memory/memory.sevice.ts
+++ api/apps/data/src/memory/memory.sevice.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PeriodEnum } from '@/apps/data/src/events/period.enum';
 import type { AppPreferences } from '@/apps/data/src/events/app-preferences.interface';
+import type { Subscription } from 'rxjs';
 
 @Injectable()
 export class MemoryService {
     private readonly clientSettings: Map<string, AppPreferences> = new Map();
+    private readonly clientSubscriptions: Map<string, Subscription[]> = new Map();
 
     updateClientSettings(id: string, currency?: string, period?: PeriodEnum) {
         const clientExists = this.clientSettings.has(id);
@@ -28,5 +30,19 @@ export class MemoryService {
 
     getClientSettings(id: string) {
         return this.clientSettings.get(id);
+    }
+
+    addClientSubscription(id: string, subscription: Subscription) {
+        let subscriptions = this.clientSubscriptions.get(id);
+        if (!subscriptions) subscriptions = [];
+        subscriptions.push(subscription);
+        this.clientSubscriptions.set(id, subscriptions);
+    }
+
+    removeClientSubscriptions(id: string) {
+        this.clientSubscriptions.get(id).forEach((s) => {
+            s.unsubscribe();
+        });
+        this.clientSubscriptions.delete(id);
     }
 }
