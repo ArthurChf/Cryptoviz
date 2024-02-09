0aad25f58c5bb72c6a1a3574b608f551e750820f
Arthur <chaff.arthur@gmail.com>
2024-02-09 01:06:17 +0100

    Fix Data Service

--- api/apps/data/src/data.service.ts
+++ api/apps/data/src/data.service.ts
@@ -1,24 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { interval } from 'rxjs';
-import { concatMap } from 'rxjs/operators';
 
 @Injectable()
 export class DataService {
-    public constructor() {
-        /* const asyncOperation = async (index: number): Promise<string> => {
-            return 'test';
-        };
-        const sub = interval(2000).pipe(concatMap(asyncOperation)).subscribe({
-            next(result) {
-                console.log(result);
-            },
-            error(e) {
-                console.log('erreuuur', e);
-            },
-            complete() {
-                console.log('finiiii');
-            }
-        });
-        // sub.unsubscribe(); */
+    public getCurrencyData() {
+        return 'currencyData';
     }
 }
--- api/apps/data/src/events/events.module.ts
+++ api/apps/data/src/events/events.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MemoryService } from '@/apps/data/src/memory/memory.sevice';
+import { DataService } from '@/apps/data/src/data.service';
 import { EventsGateway } from '@/apps/data/src/events/events.gateway';
 
 @Module({
-    providers: [MemoryService, EventsGateway]
+    providers: [DataService, MemoryService, EventsGateway]
 })
 export class EventsModule {}
