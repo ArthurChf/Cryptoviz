1c170f3de434bd371a2492679ca306caa9f50998
Antoine Romand <antoine.romand@epitech.eu>
2024-02-10 14:23:37 +0100

    Update mongodb config

--- python-scripts/src/config/mongodb_config.py
+++ python-scripts/src/config/mongodb_config.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient, UpdateOne
+from pymongo.errors import BulkWriteError
 
 class MongoDBConfig:
     def __init__(self, host, port, username, password, database):
@@ -6,30 +7,47 @@ class MongoDBConfig:
         self.port = port
         self.username = username
         self.password = password
+        self.database = database
         self.client = self.get_client()
         self.database = self.client[database]
+        
         
     
     
     def get_uri(self):
-        return f"mongodb://{self.username}:{self.password}@{self.host}"
+        return f"mongodb://{self.username}:{self.password}@{self.host}:{self.port}"
     
     def get_client(self):
-        return MongoClient(self.get_uri(), self.port)
+        try:
+            client = MongoClient(self.get_uri())
+            print(f'Connexion à la base de données {self.database} mongo réussie.')
+            return client
+        except Exception as e:
+            print(f'Erreur lors de la connexion à la base de données: {e}')
+            raise e
     
     def insert_one(self, collection_name, document):
         collection = self.database[collection_name]
         return collection.insert_one(document)
     
     def upsert_many(self, collection_name, documents):
-        collection = self.database[collection_name]
-        # Insère un document s'il n'existe pas, sinon le met à jour (éviter les doublons)
-        operations = [
-            UpdateOne(
-                {'author': doc['author'], 'title': doc['title'], 'createdAt': doc['createdAt'], 'link': doc['link']},
-                {'$set': doc},
-                upsert=True
-            ) for doc in documents
-        ]
-        return collection.bulk_write(operations)
+        try:
+            collection = self.database[collection_name]
+            operations = [
+                UpdateOne(
+                    {
+                        'author': doc.get('author', 'Inconnu'), 
+                        'title': doc.get('title', 'Sans titre'), 
+                        'createdAt': doc.get('createdAt', None), 
+                        'link': doc.get('link', 'Sans lien')
+                    },
+                    {'$set': doc},
+                    upsert=True
+                ) for doc in documents
+            ]
+            return collection.bulk_write(operations)
+        
+        except BulkWriteError as e:
+            print(f'Erreur d\'insertion des données {collection_name}: {e}')
+            return None
     
