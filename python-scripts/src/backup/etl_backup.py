20adacc4f37aaf7eeef042c99226664bb7fd6efb
Antoine Romand <antoine.romand@epitech.eu>
2024-02-10 14:25:11 +0100

    create etlbackup abstract class

---
+++ python-scripts/src/backup/etl_backup.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+import sys
+import signal
+import json
+from src.config.kafka_consumer import KafkaConsumer
+from confluent_kafka import KafkaException
+
+
+class ETLBackup(ABC):
+    def __init__(self, topic):
+        self.topic = topic
+        signal.signal(signal.SIGINT, self.signal_handler)
+        signal.signal(signal.SIGTERM, self.signal_handler)
+
+    @abstractmethod
+    def extract(self):
+        consumer = KafkaConsumer(self.topic)
+        try:
+            consumer.subscribe([self.topic])
+            print(f"Consommation du topic {self.topic}")
+            while True:
+                message = consumer.consume()
+                print(f"Message reçu: {message}")
+                if message:
+                    print(f"Message reçu: {message.value()}")
+                    try :
+                        json_message = json.loads(message.value().decode('utf-8'))
+                        self.load(json_message)
+                    except json.decoder.JSONDecodeError:
+                        print('Unable to decode message to JSON: %s', message.value())
+                    consumer.commit()
+                
+        except KafkaException as e:
+            print(f'Error while consuming {self.topic}: {e}')
+        finally:
+            consumer.close()
+        pass
+
+    @abstractmethod
+    def load(self, data):
+        pass
+    
+    def signal_handler(self, signum, frame):
+        print(f'Signal d\'arrêt reçu, fermeture du processus pour le topic {self.topic}')  
+        sys.exit(0)
