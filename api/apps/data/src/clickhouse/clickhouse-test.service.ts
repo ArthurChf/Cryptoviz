62498adf9f3499c24cc6d659b0dd8f1a0f92cedf
Antoine Romand <antoine.romand@epitech.eu>
2024-02-10 23:28:04 +0100

    add clickhouse tests

---
+++ api/apps/data/src/clickhouse/clickhouse-test.service.ts
@@ -0,0 +1,29 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ClickHouseClient } from '@depyronick/nestjs-clickhouse';
+
+@Injectable()
+export class ClickHouseTestService {
+    constructor(@Inject('CRYPTOVIZ_CLICKHOUSE_SERVER') private readonly cryptovizClickHouseServer: ClickHouseClient) {}
+
+    async testConnection() {
+        return await this.cryptovizClickHouseServer.queryPromise('SELECT 1');
+    }
+
+    async createTestTable() {
+        return await this.cryptovizClickHouseServer.queryPromise('CREATE TABLE IF NOT EXISTS cryptoviz_test (id Int32, firstname String, lastname String) ENGINE = Memory');
+    }
+
+    async insertMultipleTestRows() {
+        return await this.cryptovizClickHouseServer.queryPromise(
+            `INSERT INTO cryptoviz_test (id, firstname, lastname) VALUES (1, 'John', 'Doe'), (2, 'Jane', 'Doe');`
+        );
+    }
+
+    async selectTestTable() {
+        return await this.cryptovizClickHouseServer.queryPromise('SELECT * FROM cryptoviz_test');
+    }
+
+    async dropTestTable() {
+        return await this.cryptovizClickHouseServer.queryPromise('DROP TABLE IF EXISTS cryptoviz_test');
+    }
+}
