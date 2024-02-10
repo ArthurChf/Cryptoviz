1e2c75272601f524a6c811634c7099aeca97c893
Antoine Romand <antoine.romand@epitech.eu>
2024-02-10 14:24:46 +0100

    Change utils to common, and add folder in source

--- python-scripts/src/process/news/etl_news_transform.py
+++ python-scripts/src/process/news/etl_news_transform.py
@@ -1,10 +1,10 @@
 import pandas as pda
 import textblob as blob
 import html 
-from utils.cryptodata import cryptocurrencies_keys
+from src.common.cryptodata import cryptocurrencies_keys
 import hashlib
 
-def _analyze_content_sentiment(data):
+def __analyze_content_sentiment(data):
     content = html.unescape(data)
     blob_content = blob.TextBlob(content.lower())
     if content is None or content == "":
@@ -14,7 +14,7 @@ def _analyze_content_sentiment(data):
     normalize_sentiment = (sentiment_polarity + 1) * 50
     return normalize_sentiment
 
-def _extract_cryptocurrency_symbols(data):
+def __extract_cryptocurrency_symbols(data):
     content = html.unescape(data)
     if content is None:
         return []
@@ -27,27 +27,27 @@ def _extract_cryptocurrency_symbols(data):
     # On convertir le set en liste pour pouvoir l'utiliser dans le dataframe
     return list(found_symbols)
 
-def _generate_dataframe_from_news(data):
+def __generate_dataframe_from_news(data):
     return pda.DataFrame(data)
 
-def _reorder_dataframe_columns(dataframe):
+def __reorder_dataframe_columns(dataframe):
     columns = ['id', 'title', 'author', 'link', 'createdAt', 'content', 'sentiment', 'cryptocurrencies']
     return dataframe[columns]
 
-def _generate_id(title, link, author):
+def __generate_id(title, link, author):
     # On génère un id unique pour chaque news en utilisant le titre, le lien et l'auteur, afin d'éviter les doublons
     key = f"{title}{link}{author}"
     return hashlib.sha256(key.encode()).hexdigest()
 
 def transform_news_data(data):
-    news_dataframe = _generate_dataframe_from_news(data)
-    news_dataframe['sentiment'] = news_dataframe['content'].apply(_analyze_content_sentiment)
-    news_dataframe['cryptocurrencies'] = news_dataframe['content'].apply(_extract_cryptocurrency_symbols)
+    news_dataframe = __generate_dataframe_from_news(data)
+    news_dataframe['sentiment'] = news_dataframe['content'].apply(__analyze_content_sentiment)
+    news_dataframe['cryptocurrencies'] = news_dataframe['content'].apply(__extract_cryptocurrency_symbols)
     news_dataframe['createdAt'] = pda.to_datetime(news_dataframe['createdAt'])
     news_dataframe['createdAt'] = news_dataframe['createdAt'].apply(lambda x: x.timestamp())
-    news_dataframe['id'] = news_dataframe.apply(lambda x: _generate_id(x['title'], x['link'], x['author']), axis=1)
+    news_dataframe['id'] = news_dataframe.apply(lambda x: __generate_id(x['title'], x['link'], x['author']), axis=1)
     news_df_filtered = news_dataframe[news_dataframe['cryptocurrencies'].apply(lambda x: len(x) > 0)]
     # Reorgniser les colonnes
-    news_df_filtered = _reorder_dataframe_columns(news_df_filtered)
+    news_df_filtered = __reorder_dataframe_columns(news_df_filtered)
     return news_df_filtered.itertuples(index=False, name=None)
 
