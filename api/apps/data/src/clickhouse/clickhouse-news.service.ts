168f64e8f40fc71df688ca10d3cc4ea61a716005
Antoine Romand <antoine.romand@epitech.eu>
2024-02-11 13:35:03 +0100

    Add fear and greed function to news service (ck)

--- api/apps/data/src/clickhouse/clickhouse-news.service.ts
+++ api/apps/data/src/clickhouse/clickhouse-news.service.ts
@@ -6,9 +6,22 @@ export class ClickHouseNewsService {
     constructor(@Inject('CRYPTOVIZ_CLICKHOUSE_SERVER') private readonly cryptovizClickhouseServer: ClickHouseClient) {}
 
     async getCurrencyFearAndGreed(symbol: string) {
-        const query = '';
-        const res = this.cryptovizClickhouseServer.queryPromise(query);
-        console.log(res);
-        return res;
+        const query = `SELECT AVG(sentiment) AS sentiment
+                    FROM news nd FINAL
+                    INNER JOIN (
+                        SELECT * FROM crypto_news FINAL
+                    ) cn ON nd.id = cn.news_data_id
+                    WHERE cn.symbol = '${symbol.toLowerCase()}'
+                    AND nd.sentiment IS NOT NULL
+                    AND toDate(nd.createdAt) = today()        
+                    `;
+
+        try {
+            const res = await this.cryptovizClickhouseServer.queryPromise(query);
+            const sentiment = JSON.parse(JSON.stringify(res))[0].sentiment;
+            return sentiment;
+        } catch (error) {
+            console.error('Error executing query: ', error);
+        }
     }
 }
