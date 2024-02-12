f02e6aec01c017286bec62085e024ffe2ee52dbf
Arthur <chaff.arthur@gmail.com>
2024-02-12 19:06:17 +0100

    app : improve CurrencySelectionModal style + fetch currencies

--- app/src/components/dashboard/CurrencySelectionModal.vue
+++ app/src/components/dashboard/CurrencySelectionModal.vue
@@ -5,23 +5,28 @@
             <div class="flex flex-col mx-3 gap-12">
                 <AppSearchbar v-model="searchValue" placeholder="Search" :autofocus="true" @change="searchCurrency" @enter="searchCurrency" />
 
-                <AppLoader v-if="isSearchLoading" size="45" class="self-center stroke-subtitle" />
-                <div v-else class="scrollbar overflow-y-auto overflow-x-hidden min-h-[250px] max-h-[350px] pb-10">
-                    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
-                        <div class="group relative select-none py-4 px-5 rounded-xl border-2 transition duration-200 cursor-pointer hover:bg-background" :class="isSelectedCurrency(currency) ? 'bg-background border-active' : 'border-container hover:border-subtitle/5'" v-for="currency in currencies" :key="currency.symbol" @click="selectCurrency(currency)">
-                            <div class="absolute flex items-center gap-[5px] top-0 left-0 rounded-tl-lg rounded-br-lg px-2 transition duration-200" :class="isSelectedCurrency(currency) ? 'bg-active border-active text-background' : 'opacity-0 text-background bg-subtitle/20 border-subtitle/20 group-hover:opacity-100'">
-                                <AppIcon :name="IconEnum.CHECK" size="6" />
-                                <span class="text-xs font-semibold">{{ isSelectedCurrency(currency) ? 'Selected' : 'Select' }}</span>
-                            </div>
-                            <AppTooltip position="bottom" :text="currency.name">
-                                <div class="flex items-center gap-4 mt-2">
-                                    <AppImage :name="currency.image" size="40" />
-                                    <div class="flex flex-col gap-[3px]">
-                                        <span class="text-title font-semibold text-lg">{{ formatCurrency(currency.name) }}</span>
-                                        <span class="text-subtitle text-sm">{{ currency.symbol }}</span>
+                <div class="flex h-[250px] pb-10">
+                    <div class="flex items-center justify-center w-full text-subtitle text-xl font-semibold" v-if="!currencies.length">No results</div>
+                    <div class="w-full" v-else>
+                        <AppLoader v-if="isSearchLoading" size="45" class="stroke-subtitle" />
+                        <div v-else class="scrollbar overflow-y-auto overflow-x-hidden h-[250px] pb-10 w-full">
+                            <div class="grid items-start grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-7">
+                                <div class="group relative select-none py-4 px-5 rounded-xl border-2 transition duration-200 cursor-pointer hover:bg-background" :class="isSelectedCurrency(currency) ? 'bg-background border-active' : 'border-container hover:border-subtitle/5'" v-for="currency in currencies" :key="currency.symbol" @click="selectCurrency(currency)">
+                                    <div class="absolute flex items-center gap-[5px] top-0 left-0 rounded-tl-lg rounded-br-lg px-2 transition duration-200" :class="isSelectedCurrency(currency) ? 'bg-active border-active text-background' : 'opacity-0 text-background bg-subtitle/20 border-subtitle/20 group-hover:opacity-100'">
+                                        <AppIcon :name="IconEnum.CHECK" size="6" />
+                                        <span class="text-xs font-semibold">{{ isSelectedCurrency(currency) ? 'Selected' : 'Select' }}</span>
                                     </div>
+                                    <AppTooltip position="bottom" :text="currency.name">
+                                        <div class="flex items-start gap-4 mt-2">
+                                            <AppImage :name="currency.image" size="40" class="rounded-full shadow-md" />
+                                            <div class="flex flex-col gap-[3px]">
+                                                <span class="text-title font-semibold text-lg">{{ formatCurrency(currency.name) }}</span>
+                                                <span class="text-subtitle text-sm">{{ currency.symbol }}</span>
+                                            </div>
+                                        </div>
+                                    </AppTooltip>
                                 </div>
-                            </AppTooltip>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -39,39 +44,41 @@ import AppTooltip from '@/components/AppTooltip.vue';
 import AppSearchbar from '@/components/AppSearchbar.vue';
 import { storeToRefs } from 'pinia';
 import { useCurrencyStore } from '@/stores/currencyStore';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import type { Currency } from '@/interfaces/Currency';
 import { IconEnum } from '@/enums/IconEnum';
 import { truncate } from '@/utils/formatString';
+import { useFetchData } from '@/composables/useFetchData';
+import type { HttpOptions } from '@/interfaces/HttpOptions';
+import { HttpRouteEnum } from '@/enums/HttpRouteEnum';
 
 const currencyStore = useCurrencyStore();
 const { selectedCurrency } = storeToRefs(currencyStore);
+
+const currencies = ref<Currency[]>([]);
 
 const isOpened = defineModel<boolean>();
 const searchValue = ref('');
 const isSearchLoading = ref(false);
 
-const searchCurrency = (value: string) => {
+const fetchCurrencies = async (search: string = '') => {
     if (isSearchLoading.value) return;
     isSearchLoading.value = true;
-    // TODO : request API for currencies list
-    setTimeout(() => {
-        isSearchLoading.value = false;
-    }, 3000);
+    const httpOptions: HttpOptions = {
+        routeName: HttpRouteEnum.CRYPTO_GET_ALL_CURRENCIES,
+        queryParams: {
+            search
+        }
+    };
+    await useFetchData(httpOptions, null, (data: Currency[]) => {
+        currencies.value = data;
+    });
+    isSearchLoading.value = false;
 };
 
-const currencies = ref<Currency[]>([
-    {
-        image: 'currencies/btc.webp',
-        name: 'Bitcoin',
-        symbol: 'BTC'
-    },
-    {
-        image: 'currencies/matic.webp',
-        name: 'Polygon',
-        symbol: 'MATIC'
-    }
-]);
+const searchCurrency = async (value: string) => {
+    await fetchCurrencies(value);
+};
 
 const isSelectedCurrency = (currency: Currency) => {
     return selectedCurrency.value.image === currency.image && selectedCurrency.value.name === currency.name && selectedCurrency.value.symbol === currency.symbol;
@@ -80,8 +87,12 @@ const selectCurrency = (currency: Currency) => {
     if (!isSelectedCurrency(currency)) currencyStore.setSelectedCurrency(currency);
 };
 
-const maxCurrencyLength = 9;
+const maxCurrencyNameLength = 10;
 const formatCurrency = (currency: string) => {
-    return truncate(currency, maxCurrencyLength);
+    return truncate(currency, maxCurrencyNameLength);
 };
+
+onMounted(() => {
+    fetchCurrencies();
+});
 </script>
