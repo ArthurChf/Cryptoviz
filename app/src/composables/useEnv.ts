35d4d526c3e9adbb81cbd5c34a24d640a3a67980
Arthur <chaff.arthur@gmail.com>
2024-03-29 10:09:35 +0100

    default environment variables values

--- app/src/composables/useEnv.ts
+++ app/src/composables/useEnv.ts
@@ -1,8 +1,8 @@
 export const useEnv = () => {
     return {
-        API_URL: import.meta.env.VITE_API_URL as string,
-        API_WS_URL: import.meta.env.VITE_API_WS_URL as string,
-        ENVIRONMENT: import.meta.env.VITE_ENV as string
+        API_URL: import.meta.env.VITE_API_URL as string || '',
+        API_WS_URL: import.meta.env.VITE_API_WS_URL as string || '',
+        ENVIRONMENT: import.meta.env.VITE_ENV as string || 'DEMO'
     };
 };
 
