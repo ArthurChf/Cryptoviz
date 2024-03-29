d163e103daf712a6af6980d00af3310e0a622c25
Arthur <chaff.arthur@gmail.com>
2024-03-29 10:10:13 +0100

    set default selected currency in demo env

--- app/src/utils/getDefaultCurrency.ts
+++ app/src/utils/getDefaultCurrency.ts
@@ -1,4 +1,5 @@
 import type { Currency } from '@/interfaces/Currency';
+import { isDemoEnv } from '@/composables/useEnv';
 
 export const getDefaultCurrency = () => {
     let defaultCurrency: Currency = {
@@ -13,6 +14,12 @@ export const getDefaultCurrency = () => {
             const tmp = JSON.parse(storedCurrency);
             if (Object.hasOwn(tmp, 'name') && Object.hasOwn(tmp, 'symbol') && Object.hasOwn(tmp, 'image')) defaultCurrency = tmp;
         } catch (_) {}
+    } else if (isDemoEnv) {
+        defaultCurrency = {
+            image: 'currencies/matic.webp',
+            name: 'Polygon',
+            symbol: 'MATIC'
+        };
     }
 
     return defaultCurrency;
